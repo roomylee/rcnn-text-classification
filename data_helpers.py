47859960ee66320cbea51fb858c30a7f162fab25
roomylee <roomylee@naver.com>
2018-05-09 00:15:26 +0900

    fix data_helpers for text classification

--- data_helpers.py
+++ data_helpers.py
@@ -4,15 +4,6 @@ import nltk
 import re
 
 
-train_df = pd.read_csv("data/train.csv")
-test_df = pd.read_csv("data/test.csv")
-
-train_sentence_length = max([len(nltk.word_tokenize(x)) for x in train_df['sentence']])
-test_sentence_length = max([len(nltk.word_tokenize(x)) for x in test_df['sentence']])
-MAX_SENTENCE_LENGTH = max(train_sentence_length, test_sentence_length)
-
-LABELS_COUNT = 19
-
 def clean_str(string):
     """
     Tokenization/string cleaning for all datasets except for SST.
@@ -34,98 +25,24 @@ def clean_str(string):
     return string.strip().lower()
 
 
-def convertFile(filepath, outputpath):
-    data = []
-    lines = [line.strip() for line in open(filepath)]
-    for idx in range(0, len(lines), 4):
-        id = lines[idx].split("\t")[0]
-        relation = lines[idx + 1]
-
-        sentence = lines[idx].split("\t")[1][1:-1]
-        sentence = sentence.replace("<e1>", " _e1_ ").replace("</e1>", " _/e1_ ")
-        sentence = sentence.replace("<e2>", " _e2_ ").replace("</e2>", " _/e2_ ")
-
-        tokens = nltk.word_tokenize(sentence)
-
-        tokens.remove('_/e1_')
-        tokens.remove('_/e2_')
-
-        e1 = tokens.index("_e1_")
-        del tokens[e1]
-
-        e2 = tokens.index("_e2_")
-        del tokens[e2]
-
-        sentence = " ".join(tokens)
-        sentence = clean_str(sentence)
-
-        data.append([id, sentence, e1, e2, relation])
-
-    df = pd.DataFrame(data=data, columns=["id", "sentence", "e1_pos", "e2_pos", "relation"])
-    labelsMapping = {'Other': 0,
-                     'Message-Topic(e1,e2)': 1, 'Message-Topic(e2,e1)': 2,
-                     'Product-Producer(e1,e2)': 3, 'Product-Producer(e2,e1)': 4,
-                     'Instrument-Agency(e1,e2)': 5, 'Instrument-Agency(e2,e1)': 6,
-                     'Entity-Destination(e1,e2)': 7, 'Entity-Destination(e2,e1)': 8,
-                     'Cause-Effect(e1,e2)': 9, 'Cause-Effect(e2,e1)': 10,
-                     'Component-Whole(e1,e2)': 11, 'Component-Whole(e2,e1)': 12,
-                     'Entity-Origin(e1,e2)': 13, 'Entity-Origin(e2,e1)': 14,
-                     'Member-Collection(e1,e2)': 15, 'Member-Collection(e2,e1)': 16,
-                     'Content-Container(e1,e2)': 17, 'Content-Container(e2,e1)': 18}
-    df['label'] = [labelsMapping[r] for r in df['relation']]
-
-    df.to_csv(outputpath, index=False)
-
-
-def load_data_and_labels(path):
-    # read training data from CSV file
-    df = pd.read_csv(path)
-
-    # Text data
-    x_text = df['sentence'].tolist()
-
-    # Position data
-    dist1 = []
-    dist2 = []
-    for df_idx in range(len(df)):
-        sentence = df.iloc[df_idx]['sentence']
-        tokens = nltk.word_tokenize(sentence)
-        pos1 = df.iloc[df_idx]['e1_pos']
-        pos2 = df.iloc[df_idx]['e2_pos']
-
-        d1 = ""
-        d2 = ""
-        for word_idx in range(len(tokens)):
-            d1 += str((MAX_SENTENCE_LENGTH - 1) + word_idx - pos1) + " "
-            d2 += str((MAX_SENTENCE_LENGTH - 1) + word_idx - pos2) + " "
-        for _ in range(MAX_SENTENCE_LENGTH - len(tokens)):
-            d1 += "999 "
-            d2 += "999 "
-        dist1.append(d1)
-        dist2.append(d2)
-
-    # Label Data
-    y = df['label']
-    labels_flat = y.values.ravel()
-
-    labels_count = np.unique(labels_flat).shape[0]
-
-    # convert class labels from scalars to one-hot vectors
-    # 0  => [1 0 0 0 0 ... 0 0 0 0 0]
-    # 1  => [0 1 0 0 0 ... 0 0 0 0 0]
-    # ...
-    # 18 => [0 0 0 0 0 ... 0 0 0 0 1]
-    def dense_to_one_hot(labels_dense, num_classes):
-        num_labels = labels_dense.shape[0]
-        index_offset = np.arange(num_labels) * num_classes
-        labels_one_hot = np.zeros((num_labels, num_classes))
-        labels_one_hot.flat[index_offset + labels_dense.ravel()] = 1
-        return labels_one_hot
-
-    labels = dense_to_one_hot(labels_flat, labels_count)
-    labels = labels.astype(np.uint8)
-
-    return x_text, dist1, dist2, labels
+def load_data_and_labels(positive_data_file, negative_data_file):
+    """
+    Loads MR polarity data from files, splits the data into words and generates labels.
+    Returns split sentences and labels.
+    """
+    # Load data from files
+    positive_examples = list(open(positive_data_file, "r", encoding="UTF-8").readlines())
+    positive_examples = [s.strip() for s in positive_examples]
+    negative_examples = list(open(negative_data_file, "r", encoding="UTF-8").readlines())
+    negative_examples = [s.strip() for s in negative_examples]
+    # Split by words
+    x_text = positive_examples + negative_examples
+    x_text = [clean_str(sent) for sent in x_text]
+    # Generate labels
+    positive_labels = [[0, 1] for _ in positive_examples]
+    negative_labels = [[1, 0] for _ in negative_examples]
+    y = np.concatenate([positive_labels, negative_labels], 0)
+    return [x_text, y]
 
 
 def batch_iter(data, batch_size, num_epochs, shuffle=True):
@@ -149,12 +66,7 @@ def batch_iter(data, batch_size, num_epochs, shuffle=True):
 
 
 if __name__ == "__main__":
-    trainFile = 'SemEval2010_task8_all_data/SemEval2010_task8_training/TRAIN_FILE.TXT'
-    testFile = 'SemEval2010_task8_all_data/SemEval2010_task8_testing_keys/TEST_FILE_FULL.TXT'
+    pos_dir = "data/rt-polaritydata/rt-polarity.pos"
+    neg_dir = "data/rt-polaritydata/rt-polarity.neg"
 
-    convertFile(trainFile, "data/train.csv")
-    convertFile(testFile, "data/test.csv")
-
-    print("Train / Test file created")
-    #
-    # load_data_and_labels("data/test_google.csv")
+    load_data_and_labels(pos_dir, neg_dir)
